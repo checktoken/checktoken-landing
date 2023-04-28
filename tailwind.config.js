d8d54babb3320ea330e5e26a766a6107cea144cf
tauhidulislam100 <tauhidulislam100@gmail.com>
2023-04-28 12:00:07 +0600

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'urbanist': ['Urbanist','sans-serif'],
+        'inter': ['Inter','sans-serif'],
+        'cairo': ['Cairo','sans-serif'],
+      },
+      screens: {
+        'xs': '480px',
+      }
+    },
+  },
+  plugins: [],
+}
