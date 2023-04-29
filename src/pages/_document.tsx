ebe983b3d7c08bfa3044c63f8db7d0662e76a2bf
tauhidulislam100 <tauhidulislam100@gmail.com>
2023-04-29 11:19:51 +0600

    updated title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="scroll-smooth" lang="en">
-      <Head />
+      <Head>
+        <title>Celebcheck</title>
+      </Head>
       <body className=" scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#194E6A] scrollbar-thumb-rounded-full">
         <Main />
         <NextScript />
